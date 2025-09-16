d7813914706676eb967dd62bb8d1f64f5b39e1c2
Tania Boterman <taniaboterman@gmail.com>
2025-09-16 08:53:52 +0200

    changes made to image paths

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="grid h-svh w-screen place-items-center bg-white overflow-hidden">
       <img
-        src="/images/logo-cat-ears-01.png"
+        src="./images/logo-cat-ears-01.png"
         alt="green cat logo"
         className="max-h-72 sm:max-h-48 md:max-h-72 lg:max-h-150 w-auto object-contain"
       />
