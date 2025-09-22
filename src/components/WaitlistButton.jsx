2fd15bdae7749cd1d5e96471e63c4f0c2475dd99
Daniel Barber <d.barber@aleph-1.com>
2025-09-22 15:59:25 -0400

    Added cursor pointer to help delineate interactive button

--- src/components/WaitlistButton.jsx
+++ src/components/WaitlistButton.jsx
@@ -17,7 +17,7 @@ export default function WaitlistButton() {
   type="button"
   data-tally-open="woJJ9N"
   data-tally-auto-close="3000"
-  className="rounded-lg border border-orange-500 px-4 py-1.5 text-sm md:px-6 md:py-2 md:text-base text-gray-700 transition hover:bg-gray-300 active:bg-gray-300 active:scale-95"
+  className="rounded-lg border border-orange-500 px-4 py-1.5 text-sm md:px-6 md:py-2 md:text-base text-gray-700 transition hover:bg-gray-300 active:bg-gray-300 active:scale-95 cursor-pointer"
 >
   Join the Waitlist
 </button>
