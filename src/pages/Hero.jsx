3b28c1c6c9147eba9836ad7fe3f2bdab9e6387c8
Daniel Barber <d.barber@aleph-1.com>
2025-09-22 11:42:00 -0400

    Replace logo img with svg

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,22 +1,15 @@
 import React from "react"
+import Logo from "../assets/kindergalactic-logo.svg?react"
 import WaitlistButton from "../components/WaitlistButton"
 
-
 export default function Hero() {
-   return (
-     <div className="flex min-h-screen w-screen flex-col items-center justify-center bg-white px-4">
-      <img
-        src="./images/logo-green-cat-eyes-01.png"
-        alt="Kindergalactic green cat logo"
-        className="max-h-72 sm:max-h-48 md:max-h-72 lg:max-h-150 w-auto object-contain"
-      />
+  return (
+    <div className="flex min-h-screen w-screen flex-col items-center justify-center bg-white px-4">
+      <Logo className="max-h-72 sm:max-h-48 md:max-h-72 lg:max-h-150 w-auto object-contain" />
       <div className="mt-1 mb- flex flex-col items-center gap-3">
-     <p className="max-w-[100%] text-center text-gray-700 text-sm md:text-base leading-snug">
-  Kidswear for kids (and grownups)
-</p>
-<WaitlistButton />
-</div>
-</div>
-
-   )
+        <p className="max-w-[100%] text-center text-gray-700 text-sm md:text-base leading-snug">Streetwear for kids (and parents)</p>
+        <WaitlistButton />
+      </div>
+    </div>
+  )
 }
