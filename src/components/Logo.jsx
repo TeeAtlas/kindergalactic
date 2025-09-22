1f6665ab789593ab867d526d734d8b25c39a5c2f
Daniel Barber <d.barber@aleph-1.com>
2025-09-22 13:18:22 -0400

    Added useEffect to Logo to load JavaScript

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,5 +1,15 @@
+import { useEffect } from "react";
 import LogoSvg from "../assets/kindergalactic-logo.svg?react"
 
 export default function Logo(props) {
+  useEffect(() => {
+    const id = "logo-js";
+    if (!document.getElementById(id)) {
+      const script = document.createElement("script");
+      script.id = id;
+      script.src = "./js/logo.js";
+      document.body.appendChild(script);
+    }
+  }, []);
   return <LogoSvg className={props.className} />
 }
